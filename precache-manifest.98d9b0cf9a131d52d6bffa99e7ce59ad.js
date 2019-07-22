525cd9986c01d1ac7df8165dbc3f9f761006b7de
Praveen_Kumar <praveenprakasham@gmail.com>
2019-07-22 23:50:28 +0530

    Updates

---
+++ precache-manifest.98d9b0cf9a131d52d6bffa99e7ce59ad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb240da76aadfe9bd256d20e1caa445",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f70532e329fc0bae236f",
+    "url": "/portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "2974698be5564dfa0c23",
+    "url": "/portfolio/static/css/main.6a6e6b2e.chunk.css"
+  },
+  {
+    "revision": "f70532e329fc0bae236f",
+    "url": "/portfolio/static/js/2.f54e2b76.chunk.js"
+  },
+  {
+    "revision": "2974698be5564dfa0c23",
+    "url": "/portfolio/static/js/main.e31a709b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "077ba18932e79bb39ab0c8d555da1548",
+    "url": "/portfolio/static/media/13CSR132.077ba189.jpg"
+  },
+  {
+    "revision": "6d4228d987dc0e29f18bd90f6ab79fa4",
+    "url": "/portfolio/static/media/Praveen Resume-edited.6d4228d9.pdf"
+  },
+  {
+    "revision": "682efdc50bb53c8e4a06c803e35e1f75",
+    "url": "/portfolio/static/media/bg.682efdc5.JPG"
+  }
+]);
